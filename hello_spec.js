c3cc5e6419a2b9ab2452c2e0aa2a782da59f91ee
supabok <superbok@superbokbok.com>
2013-07-21 13:15:04 +0900

    changed to use testem & node

--- hello_spec.js
+++ hello_spec.js
@@ -1,5 +1,12 @@
-describe('hello', function(){
-  it('should say hello', function(){
-    expect(hello()).toBe('hello world');
-  });
+var test = require('tap').test;
+var hello = require('./hello');
+
+test('it says hello world', function(t){
+    t.equal(hello(), 'hello world');
+    t.end();
+});
+
+test('it says hello to person', function(t){
+    t.equal(hello('Bob'), 'hello Bob');
+    t.end();
 });
